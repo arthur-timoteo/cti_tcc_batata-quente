d738553d21bc82a70f5b7f5579c6909c004182a2
Arthur <arthur.timoteo@gmail.com>
2022-04-21 22:42:15 -0300

    TabNavigation implemented

---
+++ app_mobile/app/src/main/java/com/example/batataquente/view/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.batataquente.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.example.batataquente.R;
+import com.example.batataquente.adapter.TLAdapter;
+import com.example.batataquente.view.fragment.ClassificacaoFragment;
+import com.example.batataquente.view.fragment.ContaFragment;
+import com.example.batataquente.view.fragment.JogarFragment;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        TLAdapter tlAdapter = new TLAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        tlAdapter.addFragment(new ClassificacaoFragment(), "CLASSIFICAÇÃO");
+        tlAdapter.addFragment(new JogarFragment(), "JOGAR");
+        tlAdapter.addFragment(new ContaFragment(), "CONTA");
+        viewPager.setAdapter(tlAdapter);
+    }
+}
---
+++ app_mobile/app/src/main/java/com/example/batataquente/view/fragment/JogarFragment.java
@@ -0,0 +1,21 @@
+package com.example.batataquente.view.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.batataquente.R;
+
+public class JogarFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_jogar, container, false);
+    }
+}
